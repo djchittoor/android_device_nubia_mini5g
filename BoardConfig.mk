5b65d39e4583e2c481fa245ea8ce52eb00340392
Daniel Jacob Chittoor <djchittoor47@gmail.com>
2021-05-27 16:27:32 +0530

    mini5g: Align kernel image as per boot header v2 configuration
    Signed-off-by: Daniel Jacob Chittoor <djchittoor47@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -33,7 +33,7 @@ BOARD_BOOTIMG_HEADER_VERSION := 2
 BOARD_MKBOOTIMG_ARGS := --header_version $(BOARD_BOOTIMG_HEADER_VERSION)
 BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_CMDLINE := console=ttyMSM0,115200n8 earlycon=msm_geni_serial,0xa90000 androidboot.hardware=qcom androidboot.console=ttyMSM0 androidboot.memcg=1 lpm_levels.sleep_disabled=1 video=vfb:640x400,bpp=32,memsize=3072000 msm_rtb.filter=0x237 service_locator.enable=1 swiotlb=2048 loop.max_part=7 androidboot.usbcontroller=a600000.dwc3
-BOARD_KERNEL_IMAGE_NAME := Image-dtb
+BOARD_KERNEL_IMAGE_NAME := Image
 BOARD_KERNEL_PAGESIZE := 4096
 BOARD_KERNEL_TAGS_OFFSET := 0x00000100
 BOARD_RAMDISK_OFFSET := 0x01000000
@@ -44,7 +44,6 @@ TARGET_KERNEL_ARCH := arm64
 TARGET_KERNEL_HEADER_ARCH := arm64
 TARGET_KERNEL_VERSION := 4.14
 KERNEL_DEFCONFIG := vendor/sm8150-perf_defconfig
-
 
 # Platform
 TARGET_BOARD_PLATFORM := msmnile
